10c9a60e3bacb79faa786f5cf9916ec00744dd54
ksawi <ksawosigma@gmail.com>
2026-07-30 23:59:16 +0200

    fingerprint: fix leftover conflict markers in Session.h, UdfpsHandler.h and Android.bp
    Change-Id: I85768f1e340aadd6d9c0a4dc3f8be59f94b2d2bb

--- fingerprint/include/UdfpsHandler.h
+++ fingerprint/include/UdfpsHandler.h
@@ -7,17 +7,10 @@
 #pragma once
 
 #include <stdint.h>
-<<<<<<< HEAD
-#ifndef IMPL_V2
-#include "fingerprint.h"
-#else
-#include "fingerprint-v2.h"
-=======
 #ifndef DEVICE_USES_NEW_IMPLEMENTATION
 #include "fingerprint-legacy.h"
 #else
 #include "fingerprint.h"
->>>>>>> 2b30da8 (aidl: fingerprint: Implement new fingerprint methods)
 #endif
 
 class UdfpsHandler {
